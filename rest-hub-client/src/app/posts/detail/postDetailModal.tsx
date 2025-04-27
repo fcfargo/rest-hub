9731fd28a991e99126d40c4d38eab6b02086a47a
fcfargo <fcfargo90@gmail.com>
2025-04-27 16:52:31 +0900

    feat: add responsive css on sidebars

--- rest-hub-client/src/app/posts/detail/postDetailModal.tsx
+++ rest-hub-client/src/app/posts/detail/postDetailModal.tsx
@@ -24,6 +24,7 @@ export default function PostDetailModal({ postId }: PostDetailModalProps) {
   const { posts } = usePost();
 
   const post = posts.find((p) => p.id === postId);
+  console.log(post);
 
   if (!post || !postId) {
     return null;
